0e838d2674c1d35cf328a3fce5b7857360fc86d6
arisaghafari <arisa_ghafari@outlook.com>
2021-01-31 13:42:08 +0330

    add first app

--- appstor/apps/views.py
+++ appstor/apps/views.py
@@ -2,5 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, "apps/home.html")
+    context = {
+        "apps":[
+            {
+                "title":"arisa",
+                "description":"arisa's first app",
+                "img":"https://www.google.com/url?sa=i&url=https%3A%2F%2Fde.wikipedia.org%2Fwiki%2FApp_Store_(iOS)&psig=AOvVaw2UrVn1RZx1fMsOibu4iLAs&ust=1612174167504000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCMCmvv32xe4CFQAAAAAdAAAAABAD", 
+            }
+        ]
+    }
+    return render(request, "apps/home.html", context)
 
