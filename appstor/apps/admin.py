198e0dc6215e400112a14c3e0a40b2707de2aab1
arisaghafari <arisa_ghafari@outlook.com>
2021-02-02 18:02:25 +0330

    add desplay list, ordering, prepopulation for slug,search field in admin panel for app module

--- appstor/apps/admin.py
+++ appstor/apps/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import App
 
-admin.site.register(App)
+class AppAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'priority', 'Rate', 'status', 'created', 'updated')
+    search_fields = ('title', 'description')
+    prepopulated_fields = {'slug' : ('title',)}
+    ordering = ['priority', 'created', 'updated']
+
+admin.site.register(App, AppAdmin)
